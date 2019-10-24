815aab778b1c86fa884e6bfe5214ab9ce62107ce
afriandi <afrian@ainosi.co.id>
2019-10-24 14:33:48 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/moviegoers/ui/mainUI/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moviegoers.ui.mainUI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviegoers.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/moviegoers/ui/mainUI/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.moviegoers.ui.mainUI
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
